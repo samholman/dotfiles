14598505d0ca60354b0fc75fcb1ad5844d9ba92a
Sam Holman <sam.holman@bskyb.com>
2014-08-02 21:31:34 +0100

    Adding setup script and moving gitmodules and vimrc files.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf $DIR/.bashrc ~
+ln -sf $DIR/.gitconfig ~
+ln -sf $DIR/.tmux.conf ~
+ln -sf $DIR/.vimrc ~
+ln -sf $DIR/.vim ~
+ln -sf $DIR/.tmuxinator ~
+
+echo "Links created."
