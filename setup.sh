d9fc7581e03fedd410418bab284ede4c0af42268
Sam Holman <sam.holman@bskyb.com>
2014-08-02 23:39:29 +0100

    Updating setup script.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# These files will be linked into ~
+declare -a FILES=(
+    '.bash_profile'
+    '.bashrc'
+    '.gitconfig'
+    '.tmux.conf'
+    '.tmuxinator'
+    '.vimrc'
+    '.vim'
+)
 
-ln -sf $DIR/.bash_profile ~
-ln -sf $DIR/.bashrc ~
-ln -sf $DIR/.gitconfig ~
-ln -sf $DIR/.tmux.conf ~
-ln -sf $DIR/.vimrc ~
-ln -sf $DIR/.vim ~
-ln -sf $DIR/.tmuxinator ~
+function setup() {
+    # Find dotfiles dir
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Links created."
+    # Update repo first
+    echo "Updating..."
+    pushd $DIR
+    git pull
+    popd
+
+    # Link files
+    for f in "${FILES[@]}"; do
+        ln -sf $DIR/$f $HOME
+        echo "Linked $f"
+    done
+
+    echo "All symlinks created."
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    setup
+else
+    read -p "This will overwrite existing files in your home directory. Are you sure? [y/n] " -n 1
+    echo
+
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        setup
+    else
+        echo "Aborted."
+    fi
+fi
+
+# Finish up
+unset setup
+[ -f "$HOME/.bash_profile" ] && source "$HOME/.bash_profile"
+
